ab96c58aae79e19ef5fef171c6bce1b3a040be99
Aditvir <142807593+Aditvir@users.noreply.github.com>
2025-02-03 12:26:58 +0530

    Add files via upload

---
+++ Assignment 4/Question1.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+# Creating a 1D NumPy array with 5 elements
+arr = np.array([10, 20, 30, 40, 50])
+
+# a) Addition of 2 to all elements
+add_result = arr + 2
+print("After adding 2:", add_result)
+
+# b) Multiplication of all elements by 3
+multiply_result = arr * 3
+print("After multiplying by 3:", multiply_result)
+
+# c) Division of all elements by 2
+divide_result = arr / 2
+print("After dividing by 2:", divide_result)
